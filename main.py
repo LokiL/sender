aebe08bd94f3fd64b64e9d2c220c0fb58fba13fd
Nio <arslan.musin@fairplaystudios.ru>
2018-02-13 15:47:26 +0300

    1st functions

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 import telebot
-import botToken #contains token = '<token_here>'
-import functions
+import botToken
 
 bot = telebot.TeleBot(botToken.token)
 
-bot.polling(none_stop=False)
+@bot.message_handler(commands=['ping'])
+def send_welcome(message):
+	bot.reply_to(message, "pong")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
 
 while True:
 	pass
