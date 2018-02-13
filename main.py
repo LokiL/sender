80bc27be122e9b8ac271d4c1e6a94d908c4e0db9
Nio <arslan.musin@fairplaystudios.ru>
2018-02-13 17:29:47 +0300

    basic sending functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import telebot
 import botToken
+from os import getcwd
 
 bot = telebot.TeleBot(botToken.token)
 
@@ -8,9 +9,25 @@ bot = telebot.TeleBot(botToken.token)
 def send_welcome(message):
 	bot.reply_to(message, "pong")
 
-@bot.message_handler(func=lambda m: True)
+
+@bot.message_handler(commands=['getchatid'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, str(message.chat.id))
+
+@bot.message_handler(commands=['testsend'])
 def echo_all(message):
-	bot.reply_to(message, message.text)
+    bot.send_message(message.chat.id, "Начата загрузка файла: "+getFilePath())
+    bot.send_document(message.chat.id, sendFile())
+
+def getFilePath():
+    return getcwd() + r'\test1.apk'
+
+def sendFile():
+    docpatch = getcwd() + r'\test1.apk'
+    doc = open(docpatch, 'rb')
+    return doc
+
+
 
 bot.polling()
 
